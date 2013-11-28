c2b38097fbd402897216eb98622007c74cb8493d
TheProgramm3r <ward.programm3r@gmail.com>
2013-11-27 20:27:37 -0500

    Implementing a Level
    NOTE: DEVELOPER VERSION! UNSTABLE!

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import time
+import random
 
 pygame.init()
 pygame.font.init()
@@ -21,6 +22,7 @@ purple = (128, 0, 128)
 red = (255, 0, 0)
 silver = (192, 192, 192)
 teal = (0, 128, 128)
+deepBlue = (35, 68, 255)
 yellow = (255, 255, 0)
 
 #Screen Definition
@@ -35,11 +37,14 @@ screen_center = (screenX_center, screenY_center)
 screenSize = screenX, screenY
 
 screen = pygame.display.set_mode(screenSize)
-pygame.display.set_caption("Unnamed Ship Game - Xeo Productions")
+pygame.display.set_caption("Unnamed Ship Game - Xeo Games")
 
 #Font/Text Declaration
 
 fpsFont = pygame.font.Font(None, 15)
+
+moneyFont = pygame.font.Font(None, 15)
+goodsFont = pygame.font.Font(None, 15)
 
 #Images
 bkg = pygame.image.load("f3TAE.png")
@@ -51,6 +56,9 @@ splashRect = splash.get_rect()
 splashRect.center = screen_center
 
 Clock = pygame.time.Clock()
+
+#Variable Declaration
+goodsAvailable = 0
 
 
 #Definitions
@@ -66,9 +74,17 @@ def wipeScreenWhite():
 #Object Declaration:
 # Class for the orange dude
 class Player(object):
+
+
 	
 	def __init__(self):
-		self.rect = pygame.Rect(32, 32, 16, 16)
+		self.rect = pygame.Rect(224, 96, 32, 32)
+
+		self.atPort = False
+		self.money = 0
+		self.moneyString = ""
+		self.goodsNumber = 0
+		self.goodsNumberString = ""
 
 	def move(self, dx, dy):
 		
@@ -83,6 +99,22 @@ class Player(object):
 		# Move the rect
 		self.rect.x += dx
 		self.rect.y += dy
+
+
+		for port in ports:
+			if self.rect.colliderect(port.rect):
+				if dx > 0: # Moving right; Hit the left side of a port
+					self.rect.right = port.rect.right
+					self.atPort = True
+				if dx < 0: # Moving left; Hit the right side of a port
+					self.rect.left = port.rect.left
+					self.atPort = True
+				if dy > 0: # Moving down; Hit the top side of a port
+					self.rect.bottom = port.rect.bottom
+					self.atPort = True
+				if dy < 0: # Moving up; Hit the bottom side of a port
+					self.rect.top = port.rect.top
+					self.atPort = True
 
 		# If you collide with a block, move out based on velocity
 		for block in blocks:
@@ -100,16 +132,57 @@ class Block(object):
 	
 	def __init__(self, pos):
 		blocks.append(self)
-		self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
+		self.rect = pygame.Rect(pos[0], pos[1], 32, 32)
 
+
+class Port(Block):
+
+	def __init__(self, pos):
+		ports.append(self)
+		self.rect = pygame.Rect(pos[0], pos[1], 32, 32)
+
+
+blocks = [] # List to hold the blocks
+ports = [] # List to hold the ports
+player = Player() # Create the player
+
+# Holds the level layout in a list of strings.
+level = [
+"WWWWWWWWWWWWWWWWWWWW",
+"W                  W",
+"W                  W",
+"W             P    W",
+"W                  W",
+"W                  W",
+"W                  W",
+"W                  W",
+"W                  W",
+"W                  W",
+"W                  W",
+"W                  W",
+"W   P              W",
+"W                  W",
+"WWWWWWWWWWWWWWWWWWWW",
+]
+
+# Parse the level string above. W = wall, E = exit
+x = 155
+y = 0
+for row in level:
+    for col in row:
+        if col == "W":
+            Block((x, y))
+        if col == "P":
+            Port((x, y,))
+        x += 32
+    y += 32
+    x = 155
 
 #Program Loops
 timeVar = 0 # Amount of time that has passed
 Starting = True # Whether or not to show the splashscreen
 Running = True
 
-blocks = [] # List to hold the blocks
-player = Player() # Create the player
 
 while Running:
 	while Starting == True:
@@ -154,25 +227,49 @@ while Running:
 	#Capturing and Responding to Keys
 	key = pygame.key.get_pressed()
 	if key[pygame.K_LEFT]:
-		player.move(-2, 0)
+		player.move(-32, 0)
 	if key[pygame.K_RIGHT]:
-		player.move(2, 0)
+		player.move(32, 0)
 	if key[pygame.K_UP]:
-		player.move(0, -2)
+		player.move(0, -32)
 	if key[pygame.K_DOWN]:
-		player.move(0, 2)
+		player.move(0, 32)
 	
 	#FPS LABEL
 	fps = Clock.get_fps()
 	fps = round(fps, 2)
 	fpsString = str(fps)
-	fpsLabel = fpsFont.render(fpsString, 20, black)		
+	fpsLabel = fpsFont.render(fpsString, 20, black)	
 
+	if player.atPort == True:
+		player.money = player.money + (10*player.goodsNumber)
+		player.moneyString =  str(player.money)
+		player.goodsNumber = 0
+
+		goodsAvailable = random.randrange(1, 20)
+		player.goodsNumber = int(input("How many goods would you like to take? The max is: ", goodsAvailable, "."))
+		if player.goodsNumber > goodsAvailable:
+			player.goodsNumber = int(input("You can only take up to: ", goodsAvailable, ". Pick another number."))
+			player.goodsNumberString = str(player.goodsNumber)
+		else:
+			print("You have brought ", player.goodsNumber," goods on board.")
+			player.goodsNumberString = str(player.goodsNumber)
+
+	#Goods and Money Labels
+	goodsLabel = goodsFont.render(player.goodsNumberString, 20, black)
+	moneyLabel = moneyFont.render(player.moneyString, 20, black)
+
+
+	#Drawing to Screen
 	wipeScreenWhite()
 	screen.blit(bkg, bkgRect)
 	for block in blocks:
-		pygame.draw.rect(screen, (255, 255, 255), block.rect)
+		pygame.draw.rect(screen, (0, 0, 0), block.rect)
+	for port in ports:
+		pygame.draw.rect(screen, deepBlue, port.rect)
 	pygame.draw.rect(screen, (255, 200, 0), player.rect)
+	screen.blit(goodsLabel, (30, 10))
+	screen.blit(moneyLabel, (50, 10))
 	screen.blit(fpsLabel, (10, 10))
 	pygame.display.flip()
 	Clock.tick(30)
